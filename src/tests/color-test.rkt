f7b4fea8d7323eb87426534c0d8a08a55ad26f28
Joe Chasinga <jo.chasinga@gmail.com>
2021-08-18 11:46:33 -0700

    Change dir name

---
+++ src/tests/color-test.rkt
@@ -0,0 +1,19 @@
+#lang racket
+
+(require rackunit)
+(require rackunit/text-ui)
+(require "../color.rkt")
+
+(check-equal? (hex->rgb "FF") '(255))
+(check-equal? (hex->rgb "ff") '(255))
+(check-equal? (hex->rgb "f0") '(240))
+(check-equal? (hex->rgb "f7f7F0") '(247 247 240))
+
+(check-equal? (with-handlers ([exn:fail:contract?
+                   (λ (e) "error")])
+                (hex->rgb ""))
+              "error")
+(check-equal? (with-handlers ([exn:fail:contract?
+                   (λ (e) "error")])
+                (hex->rgb "f0f0f0f"))
+              "error")
