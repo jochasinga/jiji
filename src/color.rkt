10a2e1231ab379c84f115666cca617d2c5d9c631
Pan Chasinga <jo.chasinga@gmail.com>
2021-11-10 16:34:48 -0800

    `hex->rgb` now can accept a hex string that is 3 or more in length
    Before `hex->rgb` could only take a well-formed 6-digit hex string
    that represents a color. Now it pads the extra zeroes to i.e. "#fff".

--- src/color.rkt
+++ src/color.rkt
@@ -1,9 +1,19 @@
 #lang racket
+(require 2htdp/image)
 
-(provide hex->rgb)
+(provide (contract-out
+          [hex->rgb (-> string? list?)]
+          [hex->color (-> string? color?)]))
 
-(define MIN_HEX_DIGIT 2)
+(define MIN_HEX_DIGIT 3)
 (define MAX_HEX_DIGIT 6)
+      
+(define (pad-zero hexstr)
+  (let ([diff (- 6 (string-length hexstr))])
+    (if (< diff 0)
+        hexstr
+        (string-append hexstr (make-string diff #\0)))))
+        
 
 ;; Auxiliary function that takes care
 ;; of the nitty-gritty of parsing the string.
@@ -19,20 +29,28 @@
                         acc)])
         (aux (cdr (cdr cs)) acc_))))
 
-(define (remove-bang s)
-  (if (char=? (first (string->list s)) #\#)
-      (substring s 1)
-      s))
 
-;; Converts a color hex value to a list
-;; of RGB integer list.
+;; Remove the first '#' character from a string.
+(define (remove-bang s)
+  (let ([first-char (first (string->list s))])
+        (if (char=? first-char #\#)
+            (substring s 1)
+            s)))
+
+
+;; Converts a hex string to a color struct.
+(define (hex->color hex)
+  (apply make-color (hex->rgb)))
+
+
+;; Converts a color hex value to a list of RGB integer list.
 (define (hex->rgb hex)
   (let ([hex (remove-bang hex)])
     (if (or (< (string-length hex) MIN_HEX_DIGIT)
             (> (string-length hex) MAX_HEX_DIGIT))
         (raise-argument-error
          'hex->rgb
-         "string with length between 2 to 6"
+         "string with length between 3 to 6"
          hex)
-        (let ([cs (string->list hex)])
-          (reverse (aux (string->list hex) '()))))))
+        ;;(let ([cs (string->list (pad-zero hex))])
+         (reverse (aux (string->list (pad-zero hex)) '())))))
